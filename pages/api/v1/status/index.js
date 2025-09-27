e5a7a411f2d530c881bbfdfe39e20e670a6230de
alauracarlotta <alauracarlotta@gmail.com>
2025-09-27 18:03:07 -0300

    test: add updated_at equal parsedDate

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,10 +2,10 @@ import database from "infra/database.js";
 
 export default async function status(request, response) {
 	// controller
-	const updatedAt = Date.now();
+	const updatedAt = new Date().toISOString();
 
 	await response.status(200).json({
-		// view chave api rest (snake_case)
+		// view
 		updated_at: updatedAt,
 	});
 }
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -3,6 +3,9 @@ test("GET to /api/v1/status should return 200", async () => {
 	const response = await fetch(`${base_url}/api/v1/status`);
 	expect(response.status).toBe(200);
 
-	const bodyResponse = await response.json();
-	console.log(bodyResponse);
+	const responseBody = await response.json();
+	expect(responseBody.updated_at).toBeDefined();
+
+	const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+	expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 });
