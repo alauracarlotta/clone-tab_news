71f6e843819941dcbdc042c9b20df3b19b5ee8bb
alauracarlotta <alauracarlotta@gmail.com>
2025-11-05 19:06:38 -0300

    feat(Refs #63): map methods and add DELETE method in try-catch

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -14,29 +14,45 @@ export default async function migrations(request, response) {
 		migrationsTable: "pgmigrations",
 	};
 
-	if (request.method === "GET") {
-		const pendingMigrations = await migrationRunner(
-			defaultMigrationsConfig,
-		);
-		/* view */
-		await dbClient.end();
-		await response.status(200).json(pendingMigrations);
-	}
-
-	if (request.method === "POST") {
-		const migratedMigrations = await migrationRunner({
-			...defaultMigrationsConfig,
-			dryRun: false,
-		});
-
-		/* view */
-		await dbClient.end();
-
-		if (migratedMigrations.length > 0) {
-			await response.status(201).json(migratedMigrations);
+	try {
+		if (request.method === "GET") {
+			const pendingMigrations = await migrationRunner(
+				defaultMigrationsConfig,
+			);
+			/* view */
+			await dbClient.end();
+			await response.status(200).json(pendingMigrations);
 		}
-		await response.status(200).json(migratedMigrations);
+
+		if (request.method === "POST") {
+			const migratedMigrations = await migrationRunner({
+				...defaultMigrationsConfig,
+				dryRun: false,
+			});
+
+			/* view */
+			await dbClient.end();
+
+			if (migratedMigrations.length > 0) {
+				await response.status(201).json(migratedMigrations);
+			}
+			await response.status(200).json(migratedMigrations);
+		}
+
+		if (request.method === "DELETE") {
+			const pendingMigrations = await migrationRunner(
+				defaultMigrationsConfig,
+			);
+			/* view */
+			await dbClient.end();
+			await response.status(200).json(pendingMigrations);
+		}
+	} catch (error) {
+		console.error(error);
+		throw error;
+	} finally {
+		await dbClient.end();
 	}
 
-	return response.status(405).end();
+	// return response.status(405).end();
 }
