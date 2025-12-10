6403ba61ed22734f828b4724c00f7ad7ca636b50
alauracarlotta <alauracarlotta@gmail.com>
2025-12-09 21:49:53 -0300

    feat(refs #80): remove files not use

--- temp/teste1.js
+++
@@ -1,2 +0,0 @@
-// ! Teste1
-// revert to rash d9148ed > commit-msg (add file) > .czrc (add file) > package.json (remove code)
--- temp/teste2.js
+++
@@ -1,6 +0,0 @@
-// ! Teste1
-// revert to rash d9148ed > commit-msg (add file) > .czrc (add file) > package.json (remove code)
-
-// ! Teste2
-// add file './husky/prepare-commit-msg' > exec < /dev/tty && npx cz --hook || true
-// [bash] chmod +x .husky/prepare-commit-msg
