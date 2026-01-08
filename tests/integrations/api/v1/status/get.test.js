eefaff16d5d1f037702e728ec75be67f2a51634b
alauracarlotta <alauracarlotta@gmail.com>
2026-01-08 18:06:41 -0300

    test(refs #67): rename all descriptions to use new pattern test names

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -4,22 +4,36 @@ beforeAll(async () => {
 	await orchestrator.waitForAllServices();
 }, 60000);
 
-test("GET to /api/v1/status should return 200", async () => {
-	const response = await fetch("http://localhost:3000/api/v1/status");
-	expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+	describe("Anonymous user", () => {
+		test("Retrieving current system status", async () => {
+			const response = await fetch("http://localhost:3000/api/v1/status");
+			expect(response.status).toBe(200);
 
-	const responseBody = await response.json();
-	const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+			const responseBody = await response.json();
+			const parsedUpdateAt = new Date(
+				responseBody.updated_at,
+			).toISOString();
 
-	expect(responseBody.updated_at).toBeDefined();
-	expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+			expect(responseBody.updated_at).toBeDefined();
+			expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-	expect(responseBody.dependencies.database.version).toBeDefined();
-	expect(responseBody.dependencies.database.version).toEqual("16.10");
+			expect(responseBody.dependencies.database.version).toBeDefined();
+			expect(responseBody.dependencies.database.version).toEqual("16.10");
 
-	expect(responseBody.dependencies.database.max_connections).toBeDefined();
-	expect(responseBody.dependencies.database.max_connections).toEqual(100);
+			expect(
+				responseBody.dependencies.database.max_connections,
+			).toBeDefined();
+			expect(responseBody.dependencies.database.max_connections).toEqual(
+				100,
+			);
 
-	expect(responseBody.dependencies.database.active_connections).toBeDefined();
-	expect(responseBody.dependencies.database.active_connections).toEqual(1);
+			expect(
+				responseBody.dependencies.database.active_connections,
+			).toBeDefined();
+			expect(
+				responseBody.dependencies.database.active_connections,
+			).toEqual(1);
+		});
+	});
 });
